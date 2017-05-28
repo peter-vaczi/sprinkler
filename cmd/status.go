770c2fe9ddc309d194b929a9ee718558c215d205
Peter Vaczi <peter.vaczi@nokia.com>
2017-05-28 15:36:35 +0200

    implement the status command

--- cmd/status.go
+++ cmd/status.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"fmt"
 	"log"
+	"sort"
 
 	"github.com/spf13/cobra"
 
@@ -12,35 +14,48 @@ import (
 var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "Show status",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `Show status`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		var ret core.Devices
+		var devs core.Devices
 
-		err := utils.GetRequest(daemonSocket+"/v1/status", &ret)
+		err := utils.GetRequest(daemonSocket+"/v1/status", &devs)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		log.Printf("%v", ret)
+		printDevices(devs)
 	},
+}
+
+func printDevices(devs core.Devices) {
+	keys := make([]string, 0, len(devs))
+	for k := range devs {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	printLine([]string{"NAME", "STATUS"})
+	for _, k := range keys {
+		printDevice(devs[k])
+	}
+}
+
+func printDevice(dev *core.Device) {
+	line := make([]string, 0, 2)
+	line = append(line, dev.Name)
+	if dev.On {
+		line = append(line, "on")
+	} else {
+		line = append(line, "off")
+	}
+	printLine(line)
+}
+
+func printLine(line []string) {
+	fmt.Printf("%-20s %-5s\n", line[0], line[1])
 }
 
 func init() {
 	RootCmd.AddCommand(statusCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// statusCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// statusCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
