776e72fe1181d58113b99008ed4a73ece57e1498
Peter Vaczi <peter.vaczi@nokia.com>
2017-06-09 23:01:54 +0200

    use go-rpio

--- core/device.go
+++ core/device.go
@@ -1,15 +1,29 @@
 package core
 
-import "errors"
+import (
+	"errors"
+	"log"
+
+	rpio "github.com/stianeikeland/go-rpio"
+)
 
 var (
 	AlreadyExists = errors.New("Already exists")
 	NotFound      = errors.New("Not found")
 )
 
+func init() {
+	err := rpio.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 type Device struct {
 	Name string `json:"name"`
 	On   bool   `json:"on"`
+	Pin  int    `json:"pin"`
+	pin  rpio.Pin
 }
 
 type Devices map[string]*Device
@@ -25,6 +39,14 @@ func (d *Devices) Add(dev *Device) error {
 	}
 
 	(*d)[dev.Name] = dev
+	dev.pin = rpio.Pin(dev.Pin)
+	dev.pin.Output()
+	if dev.On {
+		dev.pin.High()
+	} else {
+		dev.pin.Low()
+	}
+
 	return nil
 }
 
@@ -39,10 +61,12 @@ func (d *Devices) Del(name string) error {
 
 func (d *Device) TurnOn() {
 	d.On = true
+	d.pin.High()
 }
 
 func (d *Device) TurnOff() {
 	d.On = false
+	d.pin.Low()
 }
 
 func (d *Device) IsOn() bool {
