f8ce699650208ab59a6775d6892633802ea31ada
Peter Vaczi <peter.vaczi@nokia.com>
2017-08-28 22:51:33 +0200

    tc refactor

--- core/eventloop_test.go
+++ core/eventloop_test.go
@@ -27,274 +27,235 @@ func sendReceive(t *testing.T, msg interface{}, err error) interface{} {
 	return resp.Body
 }
 
-func TestEventloopAddDelDevice(t *testing.T) {
-	var msg interface{}
-	var body interface{}
-
-	// add one device
-	dev := &core.Device{Name: "dev1", Pin: 1}
-	msg = core.MsgDeviceAdd{MsgRequest: core.MsgRequest{ResponseChan: responses}, Device: dev}
-	body = sendReceive(t, msg, nil)
+func addDevice(t *testing.T, name string, pin int, err error) *core.Device {
+	dev := &core.Device{Name: name, Pin: pin}
+	msg := core.MsgDeviceAdd{MsgRequest: core.MsgRequest{ResponseChan: responses}, Device: dev}
+	body := sendReceive(t, msg, err)
 	assert.Empty(t, body)
+	return dev
+}
 
-	// read device dev1
-	msg = core.MsgDeviceGet{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "dev1"}
-	body = sendReceive(t, msg, nil)
+func getDevice(t *testing.T, name string, err error) *core.Device {
+	msg := core.MsgDeviceGet{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: name}
+	body := sendReceive(t, msg, err)
+
+	if err != nil {
+		assert.Empty(t, body)
+		return nil
+	}
+
 	if assert.NotEmpty(t, body) {
 		if assert.IsType(t, &core.Device{}, body) {
-			assert.Equal(t, 1, body.(*core.Device).Pin)
+			return body.(*core.Device)
 		}
 	}
+	return nil
+}
 
-	// read a non existing device
-	msg = core.MsgDeviceGet{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "unknown-device"}
-	body = sendReceive(t, msg, core.NotFound)
+func delDevice(t *testing.T, name string, err error) {
+	msg := core.MsgDeviceDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: name}
+	body := sendReceive(t, msg, err)
 	assert.Empty(t, body)
+}
 
-	// read all devices (list of one)
-	msg = core.MsgDeviceList{MsgRequest: core.MsgRequest{ResponseChan: responses}}
-	body = sendReceive(t, msg, nil)
+func listDevices(t *testing.T) *core.Devices {
+	msg := core.MsgDeviceList{MsgRequest: core.MsgRequest{ResponseChan: responses}}
+	body := sendReceive(t, msg, nil)
+
 	if assert.NotEmpty(t, body) {
 		if assert.IsType(t, &core.Devices{}, body) {
-			d, _ := body.(*core.Devices).Get("dev1")
-			assert.NotNil(t, d)
+			return body.(*core.Devices)
 		}
 	}
+	return nil
+}
 
-	// delete dev1
-	msg = core.MsgDeviceDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "dev1"}
-	body = sendReceive(t, msg, nil)
+func addProgram(t *testing.T, name string, err error) *core.Program {
+	pr := &core.Program{Name: name}
+	msg := core.MsgProgramCreate{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: pr}
+	body := sendReceive(t, msg, err)
 	assert.Empty(t, body)
+	return pr
+}
 
-	// read dev1 (should fail, since we deleted it)
-	msg = core.MsgDeviceGet{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "dev1"}
-	body = sendReceive(t, msg, core.NotFound)
+func getProgram(t *testing.T, name string, err error) *core.Program {
+	msg := core.MsgProgramGet{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: name}
+	body := sendReceive(t, msg, err)
+
+	if err != nil {
+		assert.Empty(t, body)
+		return nil
+	}
+
+	if assert.NotEmpty(t, body) {
+		if assert.IsType(t, &core.Program{}, body) {
+			return body.(*core.Program)
+		}
+	}
+	return nil
+}
+
+func delProgram(t *testing.T, name string, err error) {
+	msg := core.MsgProgramDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: name}
+	body := sendReceive(t, msg, err)
 	assert.Empty(t, body)
+}
+
+func listPrograms(t *testing.T) *core.Programs {
+	msg := core.MsgProgramList{MsgRequest: core.MsgRequest{ResponseChan: responses}}
+	body := sendReceive(t, msg, nil)
+
+	if assert.NotEmpty(t, body) {
+		if assert.IsType(t, &core.Programs{}, body) {
+			return body.(*core.Programs)
+		}
+	}
+	return nil
+}
+
+func prAddDev(t *testing.T, pr string, dev string, dur time.Duration, err error) {
+	msg := core.MsgProgramAddDevice{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: pr, Device: dev, Duration: dur}
+	sendReceive(t, msg, err)
+}
+
+func prDelDev(t *testing.T, pr string, idx int, err error) {
+	msg := core.MsgProgramDelDevice{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: pr, Idx: idx}
+	sendReceive(t, msg, err)
+}
+
+func startProgram(t *testing.T, pr string, err error) {
+	msg := core.MsgProgramStart{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: pr}
+	sendReceive(t, msg, err)
+}
+
+func stopProgram(t *testing.T, pr string, err error) {
+	msg := core.MsgProgramStop{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: pr}
+	sendReceive(t, msg, err)
+}
+
+func TestEventloopAddDelDevice(t *testing.T) {
+	addDevice(t, "dev1", 1, nil)
+
+	dev := getDevice(t, "dev1", nil)
+	assert.Equal(t, 1, dev.Pin)
+
+	getDevice(t, "unknown-device", core.NotFound)
+
+	devs := listDevices(t)
+	assert.Equal(t, 1, len(*devs))
+	d, _ := devs.Get("dev1")
+	assert.NotNil(t, d)
+
+	delDevice(t, "dev1", nil)
+	getDevice(t, "dev1", core.NotFound)
 }
 
 func TestEventloopAddDelProgram(t *testing.T) {
-	var msg interface{}
-	var body interface{}
+	addProgram(t, "pr1", nil)
+	pr := getProgram(t, "pr1", nil)
+	assert.Equal(t, "pr1", pr.Name)
 
-	// create one program
-	pr := &core.Program{Name: "pr1"}
-	msg = core.MsgProgramCreate{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: pr}
-	body = sendReceive(t, msg, nil)
-	assert.Empty(t, body)
+	getProgram(t, "unknown-program", core.NotFound)
 
-	// read program pr1
-	msg = core.MsgProgramGet{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "pr1"}
-	body = sendReceive(t, msg, nil)
-	if assert.NotEmpty(t, body) {
-		if assert.IsType(t, &core.Program{}, body) {
-			assert.Equal(t, "pr1", body.(*core.Program).Name)
-		}
-	}
+	prgs := listPrograms(t)
+	assert.Equal(t, 1, len(*prgs))
+	d, _ := prgs.Get("pr1")
+	assert.NotNil(t, d)
 
-	// read a non existing program
-	msg = core.MsgProgramGet{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "unknown-program"}
-	body = sendReceive(t, msg, core.NotFound)
-	assert.Empty(t, body)
-
-	// read all prorams (list of one)
-	msg = core.MsgProgramList{MsgRequest: core.MsgRequest{ResponseChan: responses}}
-	body = sendReceive(t, msg, nil)
-	if assert.NotEmpty(t, body) {
-		if assert.IsType(t, &core.Programs{}, body) {
-			d, _ := body.(*core.Programs).Get("pr1")
-			assert.NotNil(t, d)
-		}
-	}
-
-	// delete pr1
-	msg = core.MsgProgramDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "pr1"}
-	body = sendReceive(t, msg, nil)
-	assert.Empty(t, body)
-
-	// read pr1 (should fail, since we deleted it)
-	msg = core.MsgProgramGet{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "pr1"}
-	body = sendReceive(t, msg, core.NotFound)
-	assert.Empty(t, body)
+	delProgram(t, "pr1", nil)
+	getProgram(t, "pr1", core.NotFound)
 }
 
 func TestEventloopAddDelDeviceToProgram(t *testing.T) {
-	var msg interface{}
+	d1 := addDevice(t, "dev1", 1, nil)
+	d2 := addDevice(t, "dev2", 2, nil)
 
-	// add one device
-	d1 := &core.Device{Name: "dev1", Pin: 1}
-	msg = core.MsgDeviceAdd{MsgRequest: core.MsgRequest{ResponseChan: responses}, Device: d1}
-	sendReceive(t, msg, nil)
+	addProgram(t, "pr1", nil)
 
-	d2 := &core.Device{Name: "dev2", Pin: 2}
-	msg = core.MsgDeviceAdd{MsgRequest: core.MsgRequest{ResponseChan: responses}, Device: d2}
-	sendReceive(t, msg, nil)
+	prAddDev(t, "pr1", "dev1", 5*time.Second, nil)
+	prAddDev(t, "pr1", "dev2", 8*time.Second, nil)
 
-	// create one program
-	pr := &core.Program{Name: "pr1"}
-	msg = core.MsgProgramCreate{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: pr}
-	sendReceive(t, msg, nil)
+	prAddDev(t, "pr1", "dev-whatever", 5*time.Second, core.NotFound)
+	prAddDev(t, "pr-whatever", "dev1", 5*time.Second, core.NotFound)
 
-	// add dev1 to pr
-	msg = core.MsgProgramAddDevice{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: "pr1", Device: "dev1", Duration: 5 * time.Second}
-	sendReceive(t, msg, nil)
-
-	// add dev2 to pr
-	msg = core.MsgProgramAddDevice{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: "pr1", Device: "dev2", Duration: 8 * time.Second}
-	sendReceive(t, msg, nil)
-
-	// add unknown dev to pr
-	msg = core.MsgProgramAddDevice{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: "pr1", Device: "dev-whatever", Duration: 5 * time.Second}
-	sendReceive(t, msg, core.NotFound)
-
-	// add dev1 to unknown pr
-	msg = core.MsgProgramAddDevice{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: "pr-whatever", Device: "dev1", Duration: 5 * time.Second}
-	sendReceive(t, msg, core.NotFound)
-
-	msg = core.MsgProgramList{MsgRequest: core.MsgRequest{ResponseChan: responses}}
-	body := sendReceive(t, msg, nil)
-	if assert.NotEmpty(t, body) {
-		if assert.IsType(t, &core.Programs{}, body) {
-			p, _ := body.(*core.Programs).Get("pr1")
-			if assert.NotNil(t, p) {
-				assert.Equal(t, len(p.Elements), 2)
-				assert.Equal(t, p.Elements[0].Device, d1)
-				assert.Equal(t, p.Elements[0].Duration, 5*time.Second)
-				assert.Equal(t, p.Elements[1].Device, d2)
-				assert.Equal(t, p.Elements[1].Duration, 8*time.Second)
-			}
-		}
+	prgs := listPrograms(t)
+	assert.Equal(t, 1, len(*prgs))
+	p, _ := prgs.Get("pr1")
+	if assert.NotNil(t, p) {
+		assert.Equal(t, len(p.Elements), 2)
+		assert.Equal(t, p.Elements[0].Device, d1)
+		assert.Equal(t, p.Elements[0].Duration, 5*time.Second)
+		assert.Equal(t, p.Elements[1].Device, d2)
+		assert.Equal(t, p.Elements[1].Duration, 8*time.Second)
 	}
 
-	// delete unknown index
-	msg = core.MsgProgramDelDevice{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: "pr1", Idx: 2}
-	sendReceive(t, msg, core.OutOfRange)
+	prDelDev(t, "pr1", 2, core.OutOfRange)
 
-	// delete all
-	msg = core.MsgProgramDelDevice{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: "pr1", Idx: 0}
-	sendReceive(t, msg, nil)
-	sendReceive(t, msg, nil)
-	sendReceive(t, msg, core.OutOfRange)
+	prDelDev(t, "pr1", 0, nil)
+	prDelDev(t, "pr1", 0, nil)
+	prDelDev(t, "pr1", 0, core.OutOfRange)
 
 	// cleanup
-	msg = core.MsgProgramDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "pr1"}
-	sendReceive(t, msg, nil)
-	msg = core.MsgDeviceDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "dev1"}
-	sendReceive(t, msg, nil)
-	msg = core.MsgDeviceDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "dev2"}
-	sendReceive(t, msg, nil)
+	delProgram(t, "pr1", nil)
+	delDevice(t, "dev1", nil)
+	delDevice(t, "dev2", nil)
 }
 
 func TestEventloopDelDeviceInUse(t *testing.T) {
-	var msg interface{}
+	addDevice(t, "dev1", 1, nil)
 
-	// add one device
-	d1 := &core.Device{Name: "dev1", Pin: 1}
-	msg = core.MsgDeviceAdd{MsgRequest: core.MsgRequest{ResponseChan: responses}, Device: d1}
-	sendReceive(t, msg, nil)
+	addProgram(t, "pr1", nil)
 
-	// create one program
-	pr := &core.Program{Name: "pr1"}
-	msg = core.MsgProgramCreate{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: pr}
-	sendReceive(t, msg, nil)
+	prAddDev(t, "pr1", "dev1", 5*time.Second, nil)
 
-	// add dev1 to pr
-	msg = core.MsgProgramAddDevice{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: "pr1", Device: "dev1", Duration: 5 * time.Second}
-	sendReceive(t, msg, nil)
+	delDevice(t, "dev1", core.DeviceInUse)
 
-	// delete dev1
-	msg = core.MsgDeviceDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "dev1"}
-	sendReceive(t, msg, core.DeviceInUse)
-
-	// delete all
-	msg = core.MsgProgramDelDevice{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: "pr1", Idx: 0}
-	sendReceive(t, msg, nil)
-	msg = core.MsgProgramDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "pr1"}
-	sendReceive(t, msg, nil)
-
-	// delete dev1
-	msg = core.MsgDeviceDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "dev1"}
-	sendReceive(t, msg, nil)
+	prDelDev(t, "pr1", 0, nil)
+	delProgram(t, "pr1", nil)
+	delDevice(t, "dev1", nil)
 }
 
 func TestEventloopStartStopProgram(t *testing.T) {
-	var msg interface{}
+	d1 := addDevice(t, "dev1", 1, nil)
+	d2 := addDevice(t, "dev2", 2, nil)
 
-	// add one device
-	d1 := &core.Device{Name: "dev1", Pin: 1}
-	msg = core.MsgDeviceAdd{MsgRequest: core.MsgRequest{ResponseChan: responses}, Device: d1}
-	sendReceive(t, msg, nil)
+	addProgram(t, "pr1", nil)
 
-	d2 := &core.Device{Name: "dev2", Pin: 2}
-	msg = core.MsgDeviceAdd{MsgRequest: core.MsgRequest{ResponseChan: responses}, Device: d2}
-	sendReceive(t, msg, nil)
-
-	// create one program
-	pr := &core.Program{Name: "pr1"}
-	msg = core.MsgProgramCreate{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: pr}
-	sendReceive(t, msg, nil)
-
-	// add dev1 to pr
-	msg = core.MsgProgramAddDevice{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: "pr1", Device: "dev1", Duration: 1 * time.Second}
-	sendReceive(t, msg, nil)
-
-	// add dev2 to pr
-	msg = core.MsgProgramAddDevice{MsgRequest: core.MsgRequest{ResponseChan: responses}, Program: "pr1", Device: "dev2", Duration: 1 * time.Second}
-	sendReceive(t, msg, nil)
+	prAddDev(t, "pr1", "dev1", 1*time.Second, nil)
+	prAddDev(t, "pr1", "dev2", 1*time.Second, nil)
 
 	// start pr
-	msg = core.MsgProgramStart{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "pr-unknown"}
-	sendReceive(t, msg, core.NotFound)
-	msg = core.MsgProgramStart{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "pr1"}
-	sendReceive(t, msg, nil)
+	startProgram(t, "pr-unknown", core.NotFound)
+	startProgram(t, "pr1", nil)
 	time.Sleep(100 * time.Millisecond)
 	assert.True(t, d1.IsOn())
 	assert.False(t, d2.IsOn())
 
 	// stop pr
-	msg = core.MsgProgramStop{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "pr-unknown"}
-	sendReceive(t, msg, core.NotFound)
-	msg = core.MsgProgramStop{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "pr1"}
-	sendReceive(t, msg, nil)
+	stopProgram(t, "pr-unknown", core.NotFound)
+	stopProgram(t, "pr1", nil)
 	time.Sleep(100 * time.Millisecond)
 	assert.False(t, d1.IsOn())
 	assert.False(t, d2.IsOn())
 
 	// cleanup
-	msg = core.MsgProgramDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "pr1"}
-	sendReceive(t, msg, nil)
-	msg = core.MsgDeviceDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "dev1"}
-	sendReceive(t, msg, nil)
-	msg = core.MsgDeviceDel{MsgRequest: core.MsgRequest{ResponseChan: responses}, Name: "dev2"}
-	sendReceive(t, msg, nil)
+	delProgram(t, "pr1", nil)
+	delDevice(t, "dev1", nil)
+	delDevice(t, "dev2", nil)
 }
 
 func TestEventloopLoadStore(t *testing.T) {
-	var msg interface{}
-	var body interface{}
-
 	core.DataFile = "file-not-found.json"
 	core.LoadState()
 
-	// read all devices (empty list)
-	msg = core.MsgDeviceList{MsgRequest: core.MsgRequest{ResponseChan: responses}}
-	body = sendReceive(t, msg, nil)
-	if assert.NotEmpty(t, body) {
-		if assert.IsType(t, &core.Devices{}, body) {
-			assert.Equal(t, 0, len(*body.(*core.Devices)))
-		}
-	}
+	devs := listDevices(t)
+	assert.Equal(t, 0, len(*devs))
 
 	core.DataFile = "data_test.json"
 	core.LoadState()
 
-	// read all devices (list of five)
-	msg = core.MsgDeviceList{MsgRequest: core.MsgRequest{ResponseChan: responses}}
-	body = sendReceive(t, msg, nil)
-	if assert.NotEmpty(t, body) {
-		if assert.IsType(t, &core.Devices{}, body) {
-			assert.Equal(t, 5, len(*body.(*core.Devices)))
-		}
-	}
+	devs = listDevices(t)
+	assert.Equal(t, 5, len(*devs))
 
 	core.DataFile = "data_test2.json"
 	core.StoreState()
